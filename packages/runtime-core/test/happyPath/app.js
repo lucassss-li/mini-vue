eca7fd6f151e257c6836aebfe9edca7477f78c93
lucassss-li <lucas8qwer@gmail.com>
2022-06-10 14:11:47 +0800

    [runtime][happy path component]

--- packages/runtime-core/test/happyPath/app.js
+++ packages/runtime-core/test/happyPath/app.js
@@ -1,6 +1,12 @@
 import { h } from '../../../../dist/vue-esm.js'
+import componentA from './componentA.js'
+import componentB from './componentB.js'
 export default {
     render() {
-        return h('div', {}, [h('h1', {}, ['hi', h('div', {}, ['lucas'])])])
+        return h('div', {}, [
+            h('h1', {}, ['hi', h('div', {}, ['lucas'])]),
+            h(componentA),
+            h(componentB),
+        ])
     },
 }
